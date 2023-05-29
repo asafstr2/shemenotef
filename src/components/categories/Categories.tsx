5844ca9e4f6a7cec4ab01deef45fe92588df91b4
asaf.strilitz <asaf.strilitz@vimeo.com>
2023-05-29 09:49:22 +0300

    adding new product

--- src/components/categories/Categories.tsx
+++ src/components/categories/Categories.tsx
@@ -1,12 +1,14 @@
 import { useGetCategoryByIdQuery } from "app/services/categoriesApi";
 import { Products, Category } from "app/types/core";
 import { CardContainer } from "components/Homepage/homepage.style";
+import { AdminButton } from "components/buttons/AdminButtons";
 import Card from "components/cards/ProductCardForHomePage";
 
 import React from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 
 function CategoryComponent() {
+  const navigate = useNavigate();
   const { categoryid } = useParams<{ categoryid: string }>();
   const {
     data: categoryData,
@@ -19,9 +21,12 @@ function CategoryComponent() {
   };
 
   if (CategoryLoading) return <h1>loading</h1>;
-  console.log({ categoryData });
   return (
     <div>
+      <AdminButton
+        handleAction={() => navigate(`/admin/addProduct`)}
+        text="הוסף מוצר חדש"
+      />{" "}
       <CardContainer>
         {categoryData.products?.map((product: Products) => (
           <Card product={product} key={product._id} />
